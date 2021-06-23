5ba511b2f36b1671afd0ffc20da595c4fbb3ae78
Navlys <secretofmyheartforyou@gmail.com>
2021-06-24 00:24:25 +0800

    sdm710-common: Drop Trust HAL

--- sdm710.mk
+++ sdm710.mk
@@ -376,10 +376,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.atrace@1.0-service.pixel
 
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
